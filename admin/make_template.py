fba129bab57390b0d71142502f996ec66f85788f
alex <alex@slab.org>
2025-03-07 13:19:01 +0000

    comments

--- admin/make_template.py
+++ admin/make_template.py
@@ -8,16 +8,18 @@ cookiejar = browser_cookie3.firefox(domain_name='doc.patternclub.org')
 url = 'https://doc.patternclub.org/new'
 headers = {'Content-type': 'text/markdown'}
 
+# read template
 file = open('../alpaca-template-2025.md')
 md = file.read()
 file.close()
 
+# create hedgedoc document
 r = requests.post(url, headers=headers, cookies=cookiejar, data=md.encode('utf-8'))
 
+# construct email
 file = open('./email.txt')
 md = file.read()
 file.close()
-
 md = md.replace("URL", r.url)
 
 print(md)
