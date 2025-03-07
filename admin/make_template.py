ba1f285a72b8288a5196fcb3c3054b4c10e2fc51
alex <alex@slab.org>
2025-03-07 13:41:27 +0000

    open in browser

--- admin/make_template.py
+++ admin/make_template.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import requests
 import browser_cookie3
+import urllib.parse
+import webbrowser
+
+if len(sys.argv) < 2:
+    print("please supply an email address")
+    sys.exit(-1)
+
+email = sys.argv[1]
+print("email:", email)
 
 # get login info from firefox
 cookiejar = browser_cookie3.firefox(domain_name='doc.patternclub.org')
@@ -23,4 +33,9 @@ md = file.read()
 file.close()
 md = md.replace("URL", r.url)
 
-print(md)
+#print(md + "\n\n")
+
+url = 'mailto:' + email + '?subject=' + urllib.parse.quote('Algorithmic Pattern talk submission templates') + '&body=' + urllib.parse.quote(md)
+
+print(url)
+webbrowser.open(url, new=0, autoraise=True)
